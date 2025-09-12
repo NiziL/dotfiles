fc4ed9d1eed2b928d6dd47e57303cd0220a13693
Théo BL <theo.biasutto-lervat@inria.fr>
2025-09-12 11:07:57 +0200

    feat(omz plugins): remove ranger, add download script

---
+++ .oh-my-zsh/custom/plugins/clone-plugins.sh
@@ -0,0 +1,3 @@
+git clone git@github.com:NiziL/ranger.plugin.zsh.git ~/.oh-my-zsh/custom/plugins/ranger
+git clone git@github.com:NiziL/iwd.plugin.zsh.git ~/.oh-my-zsh/custom/plugins/iwd
+git clone git@github.com:NiziL/usb.plugin.zsh.git ~/.oh-my-zsh/custom/plugins/usb
